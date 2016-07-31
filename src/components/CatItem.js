cc660431b115a15b3ce32002c7caf432b030e1c7
Kenneth Tsang <tsang.waiyin@gmail.com>
2016-07-31 01:01:10 -0700

    Randomize words for remove button

--- src/components/CatItem.js
+++ src/components/CatItem.js
@@ -2,13 +2,26 @@ import React from 'react';
 import './CatItem.css';
 
 const CatItem = (props) => {
+  const REMOVE_SYNONYMS = [
+    'Got it!',
+    'Cool',
+    'Good to know',
+    'Had no idea...',
+    'Didn\'t know that...',
+    'Will remember that one!',
+    'Wow!',
+    'Brilliant!',
+    'Awesome fact!'
+  ]
+  let randomWord = REMOVE_SYNONYMS[Math.floor(Math.random() * REMOVE_SYNONYMS.length)];
+
   return (
     <div className="cat-item">
       <img alt="Cat" className="cat-item__image" src={ props.image } />
       <p className="cat-item__fact">
         { props.fact }
       </p>
-      <button onClick={props.onRemoveButtonClick} type="button">Remove</button>
+      <button onClick={props.onRemoveButtonClick} type="button">{ randomWord }</button>
     </div>
   );
 }
